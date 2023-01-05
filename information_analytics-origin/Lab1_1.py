7507bb6dd02716c15a628b1b456c25223f6846a4
MukMarat22 <119050143+MukMarat22@users.noreply.github.com>
2023-01-05 23:06:32 +0500

    Add files via upload

---
+++ information_analytics-origin/Lab1_1.py
@@ -0,0 +1,68 @@
+import sys
+import PyQt5.QtSvg
+from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtWidgets import QMainWindow, QApplication, QDialog
+from PyQt5.uic import loadUi
+
+import methods_deductions
+import methods_the_middle_of_the_squares
+
+
+class Methods_and_Test(QDialog):
+    def __init__(self):
+        super(Methods_and_Test, self).__init__()
+        loadUi('IAD.ui', self)
+        self.Return = 0
+        self.setWindowTitle('Методы')
+
+        self.Methods_deductions.clicked.connect(self.methods_1)
+        self.Methods_middle_squares.clicked.connect(lambda: self.methods_2(self.Methods_middle_squares.text()))
+        # self.Methods_middle_squares.clicked.connect(self.methods_2)
+        self.Test_1.clicked.connect(self.uniformity)
+        self.Test_2.clicked.connect(lambda: self.test_2(self.Test_2.text()))
+        self.Clear.clicked.connect(self.clear)
+        self.Exit.clicked.connect(self.exit)
+
+    @QtCore.pyqtSlot()
+    def methods_1(self):
+        deductions = methods_deductions.Deductions()
+        methods = deductions.methods(0.25, 50)
+        self.textEdit_data.setText(str(methods))
+
+    def methods_2(self, text):
+        Middle_squares = methods_the_middle_of_the_squares.Middle_of_the_squares()
+        generator = Middle_squares.final_numbers(50)
+        # Middle_squares = methods_the_middle_of_the_squares.methods_the_middle_of_the_squares()
+        # methods2 = Middle_squares.methods(0.25, 50)
+        self.textEdit_data_2.setText(str(generator))
+        # self.textEdit_data_2.setText(text)
+
+    def uniformity(self):
+        deductions = methods_deductions.Deductions()
+        test_1 = deductions.uniformity(0.25, 50)
+        self.textEdit_data_3.setText(str(test_1))
+
+    def test_2(self, text):
+        Middle_squares = methods_the_middle_of_the_squares.Middle_of_the_squares()
+        Test2 = Middle_squares.independence(50)
+        self.textEdit_data_4.setText(str(Test2))
+
+    def clear(self):
+        self.textEdit_data.setText("")
+        self.textEdit_data_2.setText("")
+        self.textEdit_data_3.setText("")
+        self.textEdit_data_4.setText("")
+
+    def exit(self):
+        exit()
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = Methods_and_Test()
+    window.show()
+    app.exec_()
+
+
+if __name__ == '__main__':
+    main()
---
+++ information_analytics-origin/methods_deductions.py
@@ -0,0 +1,60 @@
+import time
+
+class Deductions:
+
+    massive_end = []
+    testing_massive = []
+    test_massive=[]
+
+    @classmethod
+    def get_massive(cls):
+        return Deductions.massive_end
+
+    def D(self,xi):
+        return float(xi)-int(xi)
+
+    def methods(self, xi, N):
+        deductions = Deductions()
+        g = 6.5
+        xi_l=xi
+
+        for n in range(N):
+            xi_n=deductions.D(xi_l)*(g*xi)
+            deductions.massive_end.append(xi_n)
+            xi_l=deductions.massive_end[n]
+            print('Итерация: ',n,', результат:',xi_n)
+
+        return deductions.massive_end
+
+    def uniformity(self,xi,N):
+        deductions=Deductions()
+        xi_l = xi
+        g = 6.5
+        B=0.9
+        for j in range(N):
+            xi_n = deductions.D(xi_l) * (g * xi)
+            deductions.testing_massive.append(xi_n)
+            xi_l = deductions.testing_massive[j]
+            p=xi_n-xi
+            X=(B-(p*N))**2/(p*N)
+            deductions.test_massive.append(X)
+
+            print('Итерация: ', j, ', результат:', X)
+
+        return deductions.test_massive
+
+
+
+
+def main():
+
+    N = 50
+    xi=0.25
+    deductions=Deductions()
+    print('\nТестирование программы запущено')
+    print('\nПервый тест:')
+    deductions.uniformity(xi,N)
+    print('\nТестированние программы завершено!')
+
+if __name__=='__main__':
+    main()
